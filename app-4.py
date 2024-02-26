99824a0aa342330a0cd9cec80d2170de09afaf0b
hihaara <161299123+hihaara@users.noreply.github.com>
2024-02-26 23:00:06 +0900

    Add files via upload

---
+++ app-4.py
@@ -0,0 +1,57 @@
+
+import streamlit as st
+import openai
+
+# Streamlit Community Cloudの「Secrets」からOpenAI API keyを取得
+openai.api_key = st.secrets.OpenAIAPI.openai_api_key
+
+system_prompt = """
+あなたは優秀な話の聞き手です。
+相談されたあらゆる物事に対して、太宰治っぽくリアクションを取ることが出来ます。
+あなたの役割は話を聞くことなので、例えば以下のようなことを聞かれても、絶対に答えないでください。
+
+* 旅行
+* 映画
+* 科学
+* 歴史
+"""
+
+# st.session_stateを使いメッセージのやりとりを保存
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [
+        {"role": "system", "content": system_prompt}
+        ]
+
+# チャットボットとやりとりする関数
+def communicate():
+    messages = st.session_state["messages"]
+
+    user_message = {"role": "user", "content": st.session_state["user_input"]}
+    messages.append(user_message)
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )
+
+    bot_message = response["choices"][0]["message"]
+    messages.append(bot_message)
+
+    st.session_state["user_input"] = ""  # 入力欄を消去
+
+
+# ユーザーインターフェイスの構築
+st.title("Dazappoido")
+st.write("ChatGPT APIを使ったお悩み相談Botです。")
+
+user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
+
+if st.session_state["messages"]:
+    messages = st.session_state["messages"]
+
+    for message in reversed(messages[1:]):  # 直近のメッセージを上に
+        speaker = "🙂"
+        if message["role"]=="assistant":
+            speaker="
+
+        st.write(speaker + ": " + message["content"])
